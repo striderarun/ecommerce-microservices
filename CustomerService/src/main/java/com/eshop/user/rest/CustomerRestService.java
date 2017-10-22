331251d32f872a1dcb61087f15b70c231f024476
Arun Mohan Subramonian <strider90arun@gmail.com>
2017-10-21 23:54:28 -0700

    Update CustomerRestService.java

--- CustomerService/src/main/java/com/eshop/user/rest/CustomerRestService.java
+++ CustomerService/src/main/java/com/eshop/user/rest/CustomerRestService.java
@@ -9,10 +9,21 @@ import java.util.List;
  * Created by arun_subramonian on 12/18/16.
  */
 public interface CustomerRestService {
-
+    /**
+    * Fetch Customer Details
+    *
+    */
     CustomerBean fetchCustomerDetails(Long id);
-
+    
+    /**
+    * Fetch customer orders
+    *
+    */
     List<CustomerOrdersBean> fetchCustomerOrders(Long id);
-
+    
+    /*
+    * Add customer
+    *
+    "/
     CustomerBean addCustomer(CustomerBean customer);
 }
